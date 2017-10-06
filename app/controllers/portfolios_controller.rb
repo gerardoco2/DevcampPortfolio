9cbcf603fc38adb2bfc374c830ab4a7892e0b19f
gerardo colina <gerardocolina@gmail.com>
2017-10-06 17:31:54 -0400

    implemented authorization rules for portfolio controller and view

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,6 +1,6 @@
 class PortfoliosController < ApplicationController
   before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
-  access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update]}, site_admin: :all
+  access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :sort]}, site_admin: :all
   layout 'portfolio'
 
 	def index

6b2098eff534de85aa8f052e33f6291ae7ad5c8e
gerardo colina <gerardocolina@gmail.com>
2017-10-07 11:16:59 -0400

    implemented initial file uploading system via carrierwae for portfolio items

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   
   get 'angular-items', to: "portfolios#angular"
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+  
 
   
   get 'about-me', to: "pages#about"
